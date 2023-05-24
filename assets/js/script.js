1829c6b416e866333160644b6f678755cc638753
Cameron Singleton <culinarycam98@gmail.com>
2023-05-23 20:30:57 -0400

    added basic vars to js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var searchHistory = [];
+var weatherApiRootUrl = "https://openweathermap.org";
+var weatherApiKey = "56888a7e0182e386acab59c0a5ef93ab";
+
+var searchForm = document.querySelector("#search-form");
+var searchInput = document.querySelector("#search-input");
+var todayContainer = document.querySelector("#today");
+var forcastContainer = document.querySelector("#forcast");
+var searchHistoryContainer = document.querySelector("#history");
